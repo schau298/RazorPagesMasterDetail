1c1ce173be3346fb04bc66dc4a40c52108747d41
carson29 <carson29@mail.nmc.edu>
2021-09-27 14:13:13 -0400

    Added sorting and filtering

--- Pages/Nate/Index.cshtml.cs
+++ Pages/Nate/Index.cshtml.cs
@@ -10,9 +10,21 @@ using RazorPagesMasterDetail.Models;
 
 namespace RazorPagesMasterDetail.Pages.Nate
 {
+    public enum SortFields
+    {
+        None,
+        Title,
+        Author,
+        Date
+    }
     public class IndexModel : PageModel
     {
         private readonly RazorPagesMasterDetail.Data.RazorPagesMasterDetailContext _context;
+
+        [BindProperty(SupportsGet =true)]
+        public string SearchString { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public SortFields SortField { get; set; }
 
         public IndexModel(RazorPagesMasterDetail.Data.RazorPagesMasterDetailContext context)
         {
@@ -23,7 +35,26 @@ namespace RazorPagesMasterDetail.Pages.Nate
 
         public async Task OnGetAsync()
         {
-            Books = await _context.Books.ToListAsync();
+            var resources = _context.Books.Select(x => x);
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                resources = resources.Where(book => book.Title.Contains(SearchString));
+            }
+            switch (SortField)
+            {
+                case SortFields.Title:
+                    resources=resources.OrderBy(book=>book.Title);
+                    break;
+                case SortFields.Author:
+                    resources = resources.OrderBy(book => book.Author);
+                    break;
+                case SortFields.Date:
+                    resources = resources.OrderBy(book => book.Date);
+                    break;
+                default:
+                    break;
+            }
+            Books = await resources.ToListAsync();
         }
     }
 }
