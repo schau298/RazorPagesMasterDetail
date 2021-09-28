207ed5ef5cbcc2f316dfa0af2b8c28a5a17cc873
BBode11 <bode11@mail.nmc.edu>
2021-09-28 18:54:47 -0400

    Developed personal views

--- Pages/Brandon/Index.cshtml.cs
+++ Pages/Brandon/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMasterDetail.Data;
 using RazorPagesMasterDetail.Models;
@@ -20,10 +21,56 @@ namespace RazorPagesMasterDetail.Pages.Brandon
         }
 
         public IList<Books> Books { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string BookGenre { get; set; }
+        public string TitleSort { get; set; }
+        public string DateSort { get; set; }
 
-        public async Task OnGetAsync()
+       
+        public async Task OnGetAsync(string sortOrder)
         {
-            Books = await _context.Books.ToListAsync();
+            TitleSort = String.IsNullOrEmpty(sortOrder) ? "TitleDesc" : "";
+            DateSort = sortOrder == "DateAsc" ? "DateDesc" : "DateAsc";
+
+            //IQueryable<string> genreQuery = from b in _context.Books
+            //                                orderby b.Genre
+            //                                select b.Genre;
+
+            var books = from b in _context.Books
+                        select b;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                books = books.Where(b => b.Title.Contains(SearchString));
+            }
+
+            //if (!string.IsNullOrEmpty(BookGenre))
+            //{
+            //    books = books.Where(x => x.Genre == BookGenre);
+            //}
+
+            switch (sortOrder)
+            {
+                case "TitleDesc":
+                    books = books.OrderByDescending(b => b.Title);
+                    break;
+                case "DateDesc":
+                    books = books.OrderByDescending(b => b.Date);
+                    break;
+                case "DateAsc":
+                    books = books.OrderBy(b => b.Date);
+                    break;
+                default:
+                    books = books.OrderBy(b => b.Title);
+                    break;
+            }
+            // Use LINQ to get list of genres.
+
+            //Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+
+            Books = await books.AsNoTracking().ToListAsync();
         }
     }
 }
